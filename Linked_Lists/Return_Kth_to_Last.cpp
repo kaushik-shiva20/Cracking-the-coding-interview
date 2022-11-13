9cca1ffb5ab748ab1eba44ab5ac687f9819e3d13
kaushik-shiva20 <64697793+kaushik-shiva20@users.noreply.github.com>
2022-11-13 13:37:44 -0500

    Add files via upload

---
+++ Linked_Lists/Return_Kth_to_Last.cpp
@@ -0,0 +1,30 @@
+/*
+ * Page: 94, Que: 2.2 - Remove Dups
+ *
+ * Notes:
+ *
+ *
+ * Mistakes:
+ *
+ * */
+
+#include "..\mLinkedList.h"
+/*
+template<class T>
+T kthtolast(mLinkedList<T> &mList, int k)
+{
+    mListNode<T> *current = mList.head;
+    int count = 0;
+    while(count < k)
+    {
+        current = current->next;
+        count++;
+    }
+    mListNode<T> *mHead = mList.head;
+    while(current)
+    {
+        current = current->next;
+        mHead = mHead->next;
+    }
+    return mHead->val;
+}*/
